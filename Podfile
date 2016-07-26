bc490774b8e677dc4dcdbf66294890a4efad764f
Keli'i Martin <kelii.d.martin@gmail.com>
2016-07-26 15:33:51 -0400

    Initial commit

---
+++ Podfile
@@ -0,0 +1,10 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+
+target 'walking-tours-manager' do
+  pod 'AFNetworking', '~> 3.0'
+  pod 'Firebase'
+  pod 'Firebase/Database'
+  pod 'Firebase/Auth'
+end
+
--- walking-tours-manager/AppDelegate.m
+++ walking-tours-manager/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+@import Firebase;
 
 @interface AppDelegate ()
 
@@ -15,8 +16,9 @@
 @implementation AppDelegate
 
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    [FIRApp configure];
     return YES;
 }
 
