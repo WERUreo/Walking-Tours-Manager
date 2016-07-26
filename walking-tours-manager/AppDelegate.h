147be207dc8dca2703a13870c17e65e65f80b2b1
Keli'i Martin <kelii.d.martin@gmail.com>
2016-07-26 15:17:50 -0400

    Initial Commit

---
+++ walking-tours-manager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  walking-tours-manager
+//
+//  Created by Keli'i Martin on 7/26/16.
+//  Copyright © 2016 WERUreo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
